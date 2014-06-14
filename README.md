998266f6e254473033c5f5d05ba285c04b539b09
Igor Mendes <igortk999@gmail.com>
2014-06-14 13:04:40 -0300

    Integrando a estrutura do servidor

---
+++ README.md
@@ -0,0 +1,17 @@
+##Aplicação de exmplo utilizando [NodeJS](http://nodejs.org) e [Express](http://expressjs.com)
+
+Vamos utilizar o o serviço do [MognoLab](https://mongolab.com). Depois vamos fazer o deploy no [Heroku](https://www.heroku.com).
+
+Para rodar localmente primeiro instale o NodeJS, depois instale o express e as dependencias com o comando:
+
+```
+npm install
+```
+
+Agora podemos iniciar o servidor
+
+```
+node server.js
+```
+
+Abra o navegador na página http://127.0.0.1:5000
---
+++ server.js
@@ -0,0 +1,153 @@
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    mongoskin  = require('mongoskin'),
+    db         = mongoskin.db('http://vitorleal:1324@ds031948.mongolab.com:31948/cabralia', { safe: true }),
+	  app        = express();
+
+
+//Adiciona header que permite requests de outros dominios
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+};
+app.use(allowCrossDomain);
+
+//Faz o parser do body para receber parâmetros dos POSTs
+app.use(bodyParser());
+
+//Adiciona a pasta public como local dos arquivos estáticos (html, css, js imagens ...)
+app.use(express.static('public'));
+
+//Adiciona o parametro collection em todas as ur
+app.param('collection', function (req, res, next, collection) {
+  req.collection = db.collection(collection);
+  return next();
+});
+
+
+/*
+ *
+ *  Páginas HTML
+ *
+ */
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/public/index.html');
+});
+
+/*
+ *
+ *  API
+ *
+ */
+//Vamos pegar as informações
+app.get('/api/:collection', function (req, res, next) {
+  //buscamos todas as informações das collections
+  req.collection.find({}, { sort: [['ordem', 1]] }).toArray(function (erro, results) {
+    if (erro) {
+      return next(erro);
+    }
+
+    //retornamos os resultados
+    res.send(results);
+  });
+});
+
+
+//Vamos inserir alguma informação na nossa collection
+app.post('/api/:collection', function(req, res, next) {
+  //pegamos os valores enviados pelo usuário
+  var json = req.body;
+
+
+  //inserimos no banco
+  req.collection.insert(json, {}, function (e, results) {
+    if (e) {
+      return next(e);
+    }
+
+    //retornamos o resultado
+    res.send(results);
+  });
+});
+
+
+//Vamos buscar um item pelo id
+app.get('/api/:collection/:id', function (req, res, next) {
+  //pagamos o parâmetro id enviado pelo usuário
+  var id = req.params.id;
+
+  //buscamos na nossa coléction pelo id enviado
+  req.collection.findById(id, function (e, result) {
+    if (e) {
+      return next(e);
+    }
+
+    //retornamos o resultado
+    res.send(result);
+  });
+});
+
+
+//Vamos buscar um item pela categoria
+app.get('/api/:collection/categoria/:categoria', function (req, res, next) {
+  //pagamos o parâmetro categoria enviado pelo usuário
+  var categoria = req.params.categoria;
+
+  //buscamos na nossa coléction pelo id enviado
+  req.collection.find({ categoria: categoria }).toArray(function (e, result) {
+    if (e) {
+      return next(e);
+    }
+
+    //retornamos o resultado
+    res.send(result);
+  });
+});
+
+
+//Vamos atualizar utilizando o id
+app.put('/api/:collection/:id', function (req, res, next) {
+  //pega a informação enciada pelo usuário
+  var json = req.body,
+      id   = req.params.id;
+
+  //faz um update buscando pelo id
+  req.collection.updateById(id, { $set: json }, { safe: true, multi:false }, function (e, result) {
+    if (e) {
+      return next(e);
+    }
+
+    if (result === 1) {
+      res.send({ msg: 'successo' });
+
+    } else {
+      res.send({ msg: 'erro' });
+    }
+  });
+});
+
+//Vamos remover um item buscando pelo id
+app.delete('/api/:collection/:id', function(req, res, next) {
+  //pagamos o parâmetro id enviado pelo usuário
+  var id = req.params.id;
+
+  //removemos o item buscando pelo id
+  req.collection.removeById(id, function (e, result) {
+    if (e) {
+      return next(e);
+    }
+
+    if (result === 1) {
+      res.send({ msg: 'successo' });
+
+    } else {
+      res.send({ msg: 'erro' });
+    }
+  });
+});
+
+
+//Roda o servidor na porta 5000
+app.listen(process.env.PORT || 5000);
+console.log('Server running at http://127.0.0.1:5000/');
