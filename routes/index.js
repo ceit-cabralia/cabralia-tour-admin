5de0a4937509a25fb3d7cbc01d501cb6120232fc
Claércio Santos <claerciomg@gmail.com>
2014-07-04 00:37:26 -0300

    CRUD completo
    Implementação do CRUD de Pontos Turísitcos

--- routes/index.js
+++ routes/index.js
@@ -1,171 +1,168 @@
 var fs = require('fs'),
-    restaurante = require('../dados/dBFuncoesR.js'),
-    ponto = require('../dados/dBFuncoes.js'),
-    transporte = require('../dados/dBFuncoesT.js');
+   restaurante = require('../dados/dBFuncoesR.js'),
+   ponto = require('../dados/dBFuncoes.js'),
+   utilidade = require('../dados/dBFuncoesU.js');
 
 exports.index = function (req, res) {
-    fs.readFile('../views/pontos.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+   fs.readFile('../views/pontos.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
 exports.novo = function (req, res) {
-    fs.readFile('../views/novoponto.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+   fs.readFile('../views/novoponto.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
 exports.criar = function (req, res) {
-    ponto.gravar(req, res);
+   ponto.gravar(req, res);
 };
 
 exports.modificar = function (req, res) {
-    ponto.modificar(req, res);
+   ponto.modificar(req, res);
 };
 
 exports.excluir = function (req, res) {
-    ponto.excluir(req, res);
+   ponto.excluir(req, res);
 };
 
 exports.editar = function (req, res) {
-    fs.readFile('../views/editarponto.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+   fs.readFile('../views/editarponto.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
 // ROTAS de Restaurantes
-
 exports.indexR = function (req, res) {
-    fs.readFile('../views/restaurantes.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+   fs.readFile('../views/restaurantes.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
 exports.novoR = function (req, res) {
-    fs.readFile('../views/novorestaurante.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+   fs.readFile('../views/novorestaurante.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
 exports.criarR = function (req, res) {
-    restaurante.gravarR(req, res);
+   restaurante.gravarR(req, res);
 };
 
 exports.modificarR = function (req, res) {
-    restaurante.modificarR(req, res);
+   restaurante.modificarR(req, res);
 };
 
 exports.excluirR = function (req, res) {
-    restaurante.excluirR(req, res);
+   restaurante.excluirR(req, res);
 };
 
 exports.editarR = function (req, res) {
-    fs.readFile('../views/editarrestaurante.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+   fs.readFile('../views/editarrestaurante.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
-
-// ROTAS de Transportes
-
-exports.indexT = function (req, res) {
-    fs.readFile('../views/transportes.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+// ROTAS de Utilidades Públicas
+exports.indexU = function (req, res) {
+   fs.readFile('../views/utilidadepublica.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
-exports.novoT = function (req, res) {
-    fs.readFile('../views/novotransporte.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+exports.novoU = function (req, res) {
+   fs.readFile('../views/novautilidade.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
 
-exports.criarT = function (req, res) {
-    transporte.gravarT(req, res);
+exports.criarU = function (req, res) {
+   utilidade.gravarU(req, res);
 };
 
-exports.modificarT = function (req, res) {
-    transporte.modificarT(req, res);
+exports.modificarU = function (req, res) {
+   utilidade.modificarU(req, res);
 };
 
-exports.excluirT = function (req, res) {
-    transporte.excluirT(req, res);
+exports.excluirU = function (req, res) {
+   utilidade.excluirU(req, res);
 };
 
-exports.editarT = function (req, res) {
-    fs.readFile('../views/editartransporte.html', function (error, content) {
-        if (error) {
-            res.writeHead(500);
-            res.end();
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.end(content, 'utf-8');
-        }
-    });
+exports.editarU = function (req, res) {
+   fs.readFile('../views/editarutilidade.html', function (error, content) {
+      if (error) {
+         res.writeHead(500);
+         res.end();
+      } else {
+         res.writeHead(200, {
+            'Content-Type': 'text/html'
+         });
+         res.end(content, 'utf-8');
+      }
+   });
 };
