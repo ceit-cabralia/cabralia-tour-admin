142cf1f7931809880706d11b2c617cb266f8b42a
Claércio Santos <claerciomg@gmail.com>
2014-07-04 00:57:58 -0300

    Implementação do CRUD de Utilidades Públicas

--- dados/dB.js
+++ dados/dB.js
@@ -1,48 +1,61 @@
 mongoose = require('mongoose');
 
-// Conex�o com o Banco 
+// Conex�o com o Banco
 
 mongoose.connect('mongodb://localhost:27017/hayoDB');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function calbckcallback() {
-    init();
+   init();
 });
 
 function init() {
-    schema = new mongoose.Schema({
-        nome: {
-            type: String,
-            unique: true
-        },
-        endereco: String,
-        descricao: String
-    });
-    pontoModel = mongoose.model('Ponto', schema);
+   schema = new mongoose.Schema({
+      nome: {
+         type: String,
+         unique: true
+      },
+      endereco: String,
+      descricao: String
+   });
+   pontoModel = mongoose.model('Ponto', schema);
 
 
-    // Esquemas de Restaurantes
-    schema = new mongoose.Schema({
-        tipo: String,
-        nome: {
-            type: String,
-            unique: true
-        },
-        endereco: String,
-        telefone: String,
-        descricao: String
-    });
-    restauranteModel = mongoose.model('Restaurante', schema);
+   // Esquemas de Restaurantes
+   schema = new mongoose.Schema({
+      tipo: String,
+      nome: {
+         type: String,
+         unique: true
+      },
+      endereco: String,
+      telefone: String,
+      descricao: String
+   });
+   restauranteModel = mongoose.model('Restaurante', schema);
 
-    // Esquemas de Transportes
-    schema = new mongoose.Schema({
-        tipo: String,
-        nome: {
-            type: String,
-            unique: true
-        },
-        telefone: String,
-        descricao: String
-    });
-    transporteModel = mongoose.model('Transporte', schema);
+   // Esquemas de Transportes
+   schema = new mongoose.Schema({
+      tipo: String,
+      nome: {
+         type: String,
+         unique: true
+      },
+      telefone: String,
+      descricao: String
+   });
+   transporteModel = mongoose.model('Transporte', schema);
+
+   // Esquemas de Utilidades Públicas
+   schema = new mongoose.Schema({
+      tipo: String,
+      nome: {
+         type: String,
+         unique: true
+      },
+      endereco: String,
+      telefone: String,
+      descricao: String
+   });
+   utilidadeModel = mongoose.model('Utilidade', schema);
 }
