7be990f98b5ac2f96430c6040c10e168a85ea377
Claércio Santos <claerciomg@gmail.com>
2014-07-04 00:45:18 -0300

    CRUD Completo
    Implementação do CRED de Utilidades Publicas

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,204 +2,272 @@
 Controllers
 */
 function ListaCtrl($scope, $http) {
-    $scope.nome = '';
+   $scope.nome = '';
 
-    $http.get('/lista/ponto').success(function (data) {
-        $scope.pontos = data;
-    })
+   $http.get('/lista/ponto').success(function (data) {
+      $scope.pontos = data;
+   })
 
-    $scope.select = function (ponto) {
-        $scope.nome = ponto.nome;
-    }
+   $scope.select = function (ponto) {
+      $scope.nome = ponto.nome;
+   }
 
-    $scope.excluir = function () {
-        if ($scope.nome != '') {
-            $http.delete('/excluir/' + $scope.nome)
-                .success(function (data) {
-                    if (data == 'err') {
-                        alert("Erro na Exclusão");
-                    } else {
-                        alert("Ponto Turistico Excluido..");
-                    }
-                    document.location.reload(true);
-                })
-        }
-    }
+   $scope.excluir = function () {
+      if ($scope.nome != '') {
+         $http.delete('/excluir/' + $scope.nome)
+            .success(function (data) {
+               if (data == 'err') {
+                  alert("Erro na Exclusão");
+               } else {
+                  alert("Ponto Turistico Excluido..");
+               }
+               document.location.reload(true);
+            })
+      }
+   }
 }
 
 function NovoCtrl($scope, $http) {
-    $scope.criar = function () {
-        $http.post('/criar', $scope.ponto)
-            .success(function (data) {
-                if (data == 'err') {
-                    alert("Erro de gravação");
-                } else {
-                    alert("Ponto Turistico cadastrado...");
-                }
-                $scope.ponto = {};
-            })
-    }
+   $scope.criar = function () {
+      $http.post('/criar', $scope.ponto)
+         .success(function (data) {
+            if (data == 'err') {
+               alert("Erro de gravação");
+            } else {
+               alert("Ponto Turistico cadastrado...");
+            }
+            $scope.ponto = {};
+         })
+   }
 }
 
 function EditarCtrl($scope, $http) {
-    var caminho = document.URL;
-    $scope._id = 0;
-    $scope.ponto = {};
+   var caminho = document.URL;
+   $scope._id = 0;
+   $scope.ponto = {};
 
-    caminho = caminho.substring(caminho.lastIndexOf('/'));
-    $http.get('/lista/ponto' + caminho).success(function (data) {
-        $scope._id = data[0]._id;
-        $scope.ponto.nome = data[0].nome;
-        $scope.ponto.endereco = data[0].endereco;
-        $scope.ponto.descricao = data[0].descricao;
-    })
+   caminho = caminho.substring(caminho.lastIndexOf('/'));
+   $http.get('/lista/ponto' + caminho).success(function (data) {
+      $scope._id = data[0]._id;
+      $scope.ponto.nome = data[0].nome;
+      $scope.ponto.endereco = data[0].endereco;
+      $scope.ponto.descricao = data[0].descricao;
+   })
 
-    $scope.modificar = function () {
-        $http.put('/modificar/' + $scope._id, $scope.ponto).success(function (data) {
-            if (data == 'err') {
-                alert("Erro na Alteração");
-            } else {
-                alert("Ponto Turistico Alterado..");
-                window.location = '/';
-            }
-        })
-    }
+   $scope.modificar = function () {
+      $http.put('/modificar/' + $scope._id, $scope.ponto).success(function (data) {
+         if (data == 'err') {
+            alert("Erro na Alteração");
+         } else {
+            alert("Ponto Turistico Alterado..");
+            window.location = '/';
+         }
+      })
+   }
 }
 
-// CRUD de CONTROLLERS para Restaurantes    
+// CRUD de CONTROLLERS para Restaurantes
 
 function ListaCtrlR($scope, $http) {
-    $scope.nome = '';
+   $scope.nome = '';
 
-    $http.get('/lista/restaurante').success(function (data) {
-        $scope.restaurantes = data;
-    })
+   $http.get('/lista/restaurante').success(function (data) {
+      $scope.restaurantes = data;
+   })
 
-    $scope.select = function (restaurante) {
-        $scope.nome = restaurante.nome;
-    }
+   $scope.select = function (restaurante) {
+      $scope.nome = restaurante.nome;
+   }
 
-    $scope.excluirR = function () {
-        if ($scope.nome != '') {
-            $http.delete('/excluirR/' + $scope.nome)
-                .success(function (data) {
-                    if (data == 'err') {
-                        alert("Erro na Exclusão de Restaurante");
-                    } else {
-                        alert("Restaurante Excluido...");
-                    }
-                    document.location.reload(true);
-                })
-        }
-    }
+   $scope.excluirR = function () {
+      if ($scope.nome != '') {
+         $http.delete('/excluirR/' + $scope.nome)
+            .success(function (data) {
+               if (data == 'err') {
+                  alert("Erro na Exclusão de Restaurante");
+               } else {
+                  alert("Restaurante Excluido...");
+               }
+               document.location.reload(true);
+            })
+      }
+   }
 }
 
 function NovoCtrlR($scope, $http) {
-    $scope.criarR = function () {
-        $http.post('/criarR', $scope.restaurante)
-            .success(function (data) {
-                if (data == 'err') {
-                    alert("Erro de gravação");
-                } else {
-                    alert("Restaurante cadastrado...");
-                }
-                $scope.restaurante = {};
-            })
-    }
+   $scope.criarR = function () {
+      $http.post('/criarR', $scope.restaurante)
+         .success(function (data) {
+            if (data == 'err') {
+               alert("Erro de gravação");
+            } else {
+               alert("Restaurante cadastrado...");
+            }
+            $scope.restaurante = {};
+         })
+   }
 }
 
 function EditarCtrlR($scope, $http) {
-    var caminho = document.URL;
-    $scope._id = 0;
-    $scope.restaurante = {};
+   var caminho = document.URL;
+   $scope._id = 0;
+   $scope.restaurante = {};
 
-    caminho = caminho.substring(caminho.lastIndexOf('/'));
-    $http.get('/lista/restaurante' + caminho).success(function (data) {
-        $scope._id = data[0]._id;
-        $scope.restaurante.tipo = data[0].tipo;
-        $scope.restaurante.nome = data[0].nome;
-        $scope.restaurante.endereco = data[0].endereco;
-        $scope.restaurante.telefone = data[0].telefone;
-        $scope.restaurante.descricao = data[0].descricao;
-    })
+   caminho = caminho.substring(caminho.lastIndexOf('/'));
+   $http.get('/lista/restaurante' + caminho).success(function (data) {
+      $scope._id = data[0]._id;
+      $scope.restaurante.tipo = data[0].tipo;
+      $scope.restaurante.nome = data[0].nome;
+      $scope.restaurante.endereco = data[0].endereco;
+      $scope.restaurante.telefone = data[0].telefone;
+      $scope.restaurante.descricao = data[0].descricao;
+   })
 
-    $scope.modificarR = function () {
-        $http.put('/modificarR/' + $scope._id, $scope.restaurante).success(function (data) {
-            if (data == 'err') {
-                alert("Erro na Alteração");
-            } else {
-                alert("Restaurante Alterado...");
-                window.location = '/R';
-            }
-        })
-    }
+   $scope.modificarR = function () {
+      $http.put('/modificarR/' + $scope._id, $scope.restaurante).success(function (data) {
+         if (data == 'err') {
+            alert("Erro na Alteração");
+         } else {
+            alert("Restaurante Alterado...");
+            window.location = '/R';
+         }
+      })
+   }
 }
 
 
-// CRUD de CONTROLLERS para Restaurantes    
+// CRUD de CONTROLLERS para Restaurantes
 
 function ListaCtrlT($scope, $http) {
-    $scope.nome = '';
+   $scope.nome = '';
 
-    $http.get('/lista/transporte').success(function (data) {
-        $scope.transportes = data;
-    })
+   $http.get('/lista/transporte').success(function (data) {
+      $scope.transportes = data;
+   })
 
-    $scope.select = function (transporte) {
-        $scope.nome = transporte.nome;
-    }
+   $scope.select = function (transporte) {
+      $scope.nome = transporte.nome;
+   }
 
-    $scope.excluirT = function () {
-        if ($scope.nome != '') {
-            $http.delete('/excluirT/' + $scope.nome)
-                .success(function (data) {
-                    if (data == 'err') {
-                        alert("Erro na Exclusão do Transporte");
-                    } else {
-                        alert("Transporte Excluido...");
-                    }
-                    document.location.reload(true);
-                })
-        }
-    }
+   $scope.excluirT = function () {
+      if ($scope.nome != '') {
+         $http.delete('/excluirT/' + $scope.nome)
+            .success(function (data) {
+               if (data == 'err') {
+                  alert("Erro na Exclusão do Transporte");
+               } else {
+                  alert("Transporte Excluido...");
+               }
+               document.location.reload(true);
+            })
+      }
+   }
 }
 
 function NovoCtrlT($scope, $http) {
-    $scope.criarT = function () {
-        $http.post('/criarT', $scope.transporte)
-            .success(function (data) {
-                if (data == 'err') {
-                    alert("Erro de gravação");
-                } else {
-                    alert("Transporte cadastrado...");
-                }
-                $scope.transporte = {};
-            })
-    }
+   $scope.criarT = function () {
+      $http.post('/criarT', $scope.transporte)
+         .success(function (data) {
+            if (data == 'err') {
+               alert("Erro de gravação");
+            } else {
+               alert("Transporte cadastrado...");
+            }
+            $scope.transporte = {};
+         })
+   }
 }
 
 function EditarCtrlT($scope, $http) {
-    var caminho = document.URL;
-    $scope._id = 0;
-    $scope.transporte = {};
+   var caminho = document.URL;
+   $scope._id = 0;
+   $scope.transporte = {};
 
-    caminho = caminho.substring(caminho.lastIndexOf('/'));
-    $http.get('/lista/transporte' + caminho).success(function (data) {
-        $scope._id = data[0]._id;
-        $scope.transporte.tipo = data[0].tipo;
-        $scope.transporte.nome = data[0].nome;
-        $scope.transporte.telefone = data[0].telefone;
-        $scope.transporte.descricao = data[0].descricao;
-    })
+   caminho = caminho.substring(caminho.lastIndexOf('/'));
+   $http.get('/lista/transporte' + caminho).success(function (data) {
+      $scope._id = data[0]._id;
+      $scope.transporte.tipo = data[0].tipo;
+      $scope.transporte.nome = data[0].nome;
+      $scope.transporte.telefone = data[0].telefone;
+      $scope.transporte.descricao = data[0].descricao;
+   })
 
-    $scope.modificarT = function () {
-        $http.put('/modificarT/' + $scope._id, $scope.transporte).success(function (data) {
+   $scope.modificarT = function () {
+      $http.put('/modificarT/' + $scope._id, $scope.transporte).success(function (data) {
+         if (data == 'err') {
+            alert("Erro na Alteração");
+         } else {
+            alert("Transporte Alterado...");
+            window.location = '/T';
+         }
+      })
+   }
+}
+
+// CRUD de CONTROLLERS para Utilidades Públicas
+function ListaCtrlU($scope, $http) {
+   $scope.nome = '';
+
+   $http.get('/lista/utilidade').success(function (data) {
+      $scope.utilidades = data;
+   })
+
+   $scope.select = function (utilidade) {
+      $scope.nome = utilidade.nome;
+   }
+
+   $scope.excluirU = function () {
+      if ($scope.nome != '') {
+         $http.delete('/excluirU/' + $scope.nome)
+            .success(function (data) {
+               if (data == 'err') {
+                  alert("Erro na Exclusão da Utilidadae Pública");
+               } else {
+                  alert("Utilidade Pública Excluido...");
+               }
+               document.location.reload(true);
+            })
+      }
+   }
+}
+
+function NovoCtrlU($scope, $http) {
+   $scope.criarU = function () {
+      $http.post('/criarU', $scope.utilidade)
+         .success(function (data) {
             if (data == 'err') {
-                alert("Erro na Alteração");
+               alert("Erro de Cadastro da Utilidade Pública");
             } else {
-                alert("Transporte Alterado...");
-                window.location = '/T';
+               alert("Utilidadae Pública cadastrado...");
             }
-        })
-    }
+            $scope.utilidade = {};
+         })
+   }
+}
+
+function EditarCtrlU($scope, $http) {
+   var caminho = document.URL;
+   $scope._id = 0;
+   $scope.utilidade = {};
+
+   caminho = caminho.substring(caminho.lastIndexOf('/'));
+   $http.get('/lista/utilidade' + caminho).success(function (data) {
+      $scope._id = data[0]._id;
+      $scope.utilidade.tipo = data[0].tipo;
+      $scope.utilidade.nome = data[0].nome;
+      $scope.utilidade.endereco = data[0].endereco;
+      $scope.utilidade.telefone = data[0].telefone;
+      $scope.utilidade.descricao = data[0].descricao;
+   })
+
+   $scope.modificarU = function () {
+      $http.put('/modificarU/' + $scope._id, $scope.utilidade).success(function (data) {
+         if (data == 'err') {
+            alert("Erro na Alteração da utilidade Pública");
+         } else {
+            alert("Utilidade Pública Alterado...");
+            window.location = '/U';
+         }
+      })
+   }
 }
--- server/server.js
+++ server/server.js
@@ -1,31 +1,33 @@
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development'
 
 var express = require('express'),
-    routes = require('../routes'),
-    ponto = require('../dados/dBFuncoes.js'),
-    restaurante = require('../dados/dBFuncoesR.js'),
-    transporte = require('../dados/dBFuncoesT.js');
+   routes = require('../routes'),
+   ponto = require('../dados/dBFuncoes.js'),
+   restaurante = require('../dados/dBFuncoesR.js'),
+   transporte = require('../dados/dBFuncoesT.js'),
+   utilidade = require('../dados/dBFuncoesU.js');
+
 
 var app = module.exports = express.createServer();
 
 app.configure(function () {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use('/css', express.static(__dirname + './../public/stylesheets'));
-    app.use('/js', express.static(__dirname + './../public/javascripts'));
-    app.use('/img', express.static(__dirname + './../public/images/'));
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
+   app.use('/css', express.static(__dirname + './../public/stylesheets'));
+   app.use('/js', express.static(__dirname + './../public/javascripts'));
+   app.use('/img', express.static(__dirname + './../public/images/'));
 });
 
 // Ambientes
 app.configure('development', function () {
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
+   app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+   }));
 });
 app.configure('production', function () {
-    app.use(express.errorHandler());
+   app.use(express.errorHandler());
 });
 
 // Rotas do Sistema
@@ -59,6 +61,16 @@ app.put('/modificarT/:id', routes.modificarT);
 app.delete('/excluirT/:nom', routes.excluirT);
 app.get('/lista/transporte', transporte.listarT);
 app.get('/lista/transporte/:nom', transporte.procurarT);
+
+// Rotas das Utilidades Públicas
+app.get('/U', routes.indexU);
+app.get('/novoU', routes.novoU);
+app.post('/criarU', routes.criarU);
+app.get('/editarU/:nom', routes.editarU);
+app.put('/modificarU/:id', routes.modificarU);
+app.delete('/excluirU/:nom', routes.excluirU);
+app.get('/lista/utilidade', utilidade.listarU);
+app.get('/lista/utilidade/:nom', utilidade.procurarU);
 
 app.listen(3000);
 console.log("Servidor está executando na porta %d em modo de %s", app.address().port, app.settings.env);
