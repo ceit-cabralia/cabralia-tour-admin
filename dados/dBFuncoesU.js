c19bb8da570a9002f7579814fca6dd0e24b74c0c
Claércio Santos <claerciomg@gmail.com>
2014-07-04 00:32:11 -0300

    CRUD Completo
    Implementação do CRUD de Utilidades Públicas

---
+++ dados/dBFuncoesU.js
@@ -0,0 +1,69 @@
+banco = require('./dB.js'),
+//Funções de  CRUD para utilidades
+
+exports.listarU = function (req, res) {
+   utilidadeModel.find().exec(function (err, utilidade) {
+      if (err) {
+         return console.log(err);
+      }
+      return res.json(utilidade);
+   });
+}
+
+exports.gravarU = function (req, res) {
+   var novaUtilidade = new utilidadeModel({
+      tipo: req.body.tipo,
+      nome: req.body.nome,
+      endereco: req.body.endereco,
+      telefone: req.body.telefone,
+      descricao: req.body.descricao
+   });
+   novaUtilidade.save(function (err) {
+      if (err) {
+         callback(err);
+      } else {
+         res.send();
+      }
+   });
+}
+
+exports.procurarU = function (req, res) {
+   utilidadeModel.find({
+      nome: req.params.nom
+   }).exec(function (err, utilidade) {
+      if (err) {
+         return console.log(err);
+      }
+      return res.json(utilidade);
+   });
+}
+
+exports.modificarU = function (req, res) {
+   utilidadeModel.update({
+      _id: req.params.id
+   }, {
+      tipo: req.body.tipo,
+      nome: req.body.nome,
+      endereco: req.body.endereco,
+      telefone: req.body.telefone,
+      descricao: req.body.descricao
+   }).exec(function (err) {
+      if (err) {
+         callback(err);
+      } else {
+         res.send();
+      }
+   });
+}
+
+exports.excluirU = function (req, res) {
+   utilidadeModel.remove({
+      nome: req.params.nom
+   }).exec(function (err) {
+      if (err) {
+         callback(err);
+      } else {
+         res.send();
+      }
+   });
+}
